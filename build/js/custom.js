f1b344c8a9abee5dffb30f95f29fba9c0a284b91
Michael Vlasov <vlasovmichael88@gmail.com>
2019-10-24 10:46:58 +0300

    js  & html done

--- build/js/custom.js
+++ build/js/custom.js
@@ -1,63 +1,76 @@
-// init select2
-// $('.js-select2:not([multiple])').select2({
-//     width: '100%',
-//     minimumResultsForSearch: 30,
-//     placeholder: $(this).data('placeholder'),
-//     templateResult: formatState
-// });
+const resultEl = document.querySelector('.main-form_result__input');
+const lengthEl = document.querySelector('.main-form-options-list__number');
+const lowerEl = document.querySelector('.main-form-options-list__lower');
+const upperEl = document.querySelector('.main-form-options-list__upper');
+const numberEl = document.querySelector('.main-form-options-list__numbers');
+const symbolsEl = document.querySelector('.main-form-options-list__symbols');
+const generateEl = document.querySelector('.main-form__btn');
+const clipbordEl = document.querySelector('.main-form_result__copy');
 
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
 
-// header menu
-// $('.header__buter, .js-menu__close').click(function(){
-//     $('.header__buter').toggleClass('is-active');
-//     $('.header__menu').toggleClass('is-open');
-// })
+generateEl.addEventListener('click', (e) => {
+    e.preventDefault();
+    const length = +lengthEl.value;
+    const hasUpper = upperEl.checked;
+    const hasLower = lowerEl.checked;
+    const hasNumbers = numberEl.checked;
+    const hasSymbols = symbolsEl.checked;
 
+    resultEl.innerText = generatePassword(hasUpper, hasLower, hasNumbers, hasSymbols, length);
+});
 
-// $(document).on('click touchstart', function(event) {
-//     if (!$(event.target).closest('.header').length) {
-//         $('.header__buter').removeClass('is-active');
-//         $('.header__menu').removeClass('is-open');
-//     }
-// });
+clipbordEl.addEventListener('click', () => {
+    const textarea = document.createElement('textarea');
+    const password = resultEl.innerText;
+    if (!password) {
+        return;
+    }
+    textarea.value = password;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    textarea.remove();
+});
 
-// validation
-// $('.js-form-submit').on('click', function() {
-//     var el = $('.js-form-input');
-//     if($(el).val() == 0) {
-//         el.addClass('not-valid');
-//     }
-//     else{
-//         el.removeClass('not-valid');
-//     }
-// });
+function generatePassword(lower, upper, number, symbol, length) {
+    let generatedPassword = '';
+    const typesCount = lower + upper + number + symbol;
+    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
 
-// reviews slider 
-// $('.js-reviews').slick({
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     autoplay: false,
-//     autoplaySpeed: 5000,
-//     draggable: false,
-//     arrows: true,
-//     responsive: [
-//     {
-//       breakpoint: 1281,
-//       settings: {
-//         slidesToShow: 2,
-//         slidesToScroll: 2,
-//         arrows: true,
-//         adaptiveHeight: true
-//       }
-//     },
-//     {
-//       breakpoint: 480,
-//       settings: {
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         arrows: true,
-//         adaptiveHeight: true
-//       }
-//     }
-//   ]
-// });
+    if (typesCount === 0) {
+        return '';
+    }
+
+    for ( let i = 0; i < length; i+=typesCount ) {
+        typesArr.forEach(type => {
+            const funcName = Object.keys(type)[0];
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    const finalPass = generatedPassword.slice(0, length);
+    return finalPass;
+}
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 48);
+}
+
+function getRandomSymbol() {
+    const symbols = '!@#$%^&*(){}[]=<>/,.';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
